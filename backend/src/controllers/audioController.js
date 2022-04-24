9748e0b73d3544d34cbab2b5c0de87ef68560b7f
Gabriel Keilty <gabrielpkeilty@gmail.com>
2022-04-24 17:34:06 -0300

    fix: bug in update and delete controllers

--- backend/src/controllers/audioController.js
+++ backend/src/controllers/audioController.js
@@ -2,90 +2,68 @@ const db = require("../database/models");
 const path = require('path');
 
 module.exports = {
-  audioCreate: async (req, res) => {
-    try {
-      const { theme_id, recipient_id } = req.body;
-      await db.Audio.create({
-        name: req.file.filename,
-        theme_id: theme_id,
-        recipient_id: recipient_id,
-      });
-      res.status(200).send({ msg: "The audio was created" });
-    } catch (error) {
-      // next(error);
-      res.status(500).send({ msg: "There was an error" });
-    }
-  },
+  audioCreate: (req, res) => {
 
-  audioDestroy: async (req, res) => {
-    const { id } = req.params;
-    try {
-      await db.Audio.destroy({
-        where: { id: id },
-      });
-      let recipients = db.Recipient.findAll()
-      let themes = db.Theme.findAll()
-      let audios = db.Audio.findAll({
-      include: ['theme', 'recipient']
-    }) 
-
-      Promise.all([recipients, themes, audios])
-      .then(([recipients, themes, audios]) => {
-          return res.render('index', { 
-              recipients, 
-              themes,
-              audios            
-          })
+    const { theme_id, recipient_id } = req.body;
+    db.Audio.create({
+      name: req.file.filename,
+      theme_id: theme_id,
+      recipient_id: recipient_id,
+    })
+      .then(() => {
+        res.status(200).send({ msg: "The audio was created" });
       })
-      // res.status(200).send({ msg: "The audio has been deleted" });
-    } catch (error) {
-      res.status(500).send({ msg: "The audio has not been deleted" });
-    }
+      .catch((error) => {
+        res.status(500).send({ msg: "There was an error" });
+      })
   },
 
-  audioUpdate: async (req, res) => {      
+  audioDestroy: (req, res) => {
+
+    const { id } = req.params;
+    db.Audio.destroy({
+      where: {
+        id: id
+      }
+    })
+      .then(() => {
+        return res.redirect('/')
+      })
+      .catch((error) => {
+        res.send(error)
+      })
+  },
+
+  audioUpdate: (req, res) => {
 
     const { id } = req.params;
     const { theme_id } = req.body;
-    try {
-      await db.Audio.update(
-        {
-          theme_id: theme_id,
+    db.Audio.update(
+      {
+        theme_id: theme_id,
+      },
+      {
+        where: {
+          id: id,
         },
-        {
-          where: {
-            id: id,
-          },
-        }
-      );
-
-      let recipients = db.Recipient.findAll()
-      let themes = db.Theme.findAll()
-      let audios = db.Audio.findAll({
-      include: ['theme', 'recipient']
-    }) 
-
-      Promise.all([recipients, themes, audios])
-      .then(([recipients, themes, audios]) => {
-          return res.render('index', { 
-              recipients, 
-              themes,
-              audios            
-          })
       })
-      // res.status(201).send({ msg: "The audio has been updated" });
-    } catch (error) {
-      res.status(500).send(error);
-    }
+      .then(() => {
+        return res.redirect('/')
+      })
+      .catch((error) => {
+        res.send(error)
+      })
   },
 
-  audioList: async (req, res) => {
-    try {
-      const audioList = await db.Audio.findAll();
-      res.status(200).json(audioList);
-    } catch (error) {
-      next(error);
-    }
+  audioList: (req, res) => {
+
+    db.Audio.findAll()
+      .then(() => {
+        return res.status(200).json(audioList);
+      })
+      .catch((error) => {
+        res.send(error)
+      })
   },
   getIndexHtml: (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
